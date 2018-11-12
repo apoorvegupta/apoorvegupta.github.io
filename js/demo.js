db3c7f4e870922aae3221b46bb1447109cc1b4a8
Apoorve Gupta <43611273+apoorvegupta@users.noreply.github.com>
2018-11-13 00:07:05 +0530

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+	
+    dotColor: '#16bcb4',
+    lineColor: '#16bcb4'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
